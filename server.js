07c183ab340414ce1a187ffcc4ae8f5b631b99f1
Silambarasan RD <63279453+silambarasan-rd@users.noreply.github.com>
2021-06-24 03:53:25 +0000

    Pagination implementation

--- server.js
+++ server.js
@@ -72,35 +72,43 @@ app.post('/api/addGeneralKnowledge', (req, res) => {
 })
 
 // List all GK row
-app.get('/api/listGK', (req, res) => {
+app.get('/api/listGK', async (req, res) => {
   const params = req.query
   console.log(params)
   
   let cond = {};
+  let orderCond = {};
+  let orderStrategy = 1; // 1 = Asc, -1 Desc
+  let pageCount = 10, pageNumber = 0;
+
+  if(params.page !== undefined && params.page !== null && params.page !== '') {
+    pageNumber = params.page;
+  }
+
   if(Object.keys(params).length != 0) {
     let dated = {};
-    if(params.from !== undefined && params.from !== null) {
+    if(params.from !== undefined && params.from !== null && params.from !== '') {
       dated = {
         ...dated,
         $gte: new Date(params.from),
       };      
     }
 
-    if(params.to !== undefined && params.to !== null) {
+    if(params.to !== undefined && params.to !== null && params.to !== '') {
       dated = {
         ...dated,
         $lte: new Date(params.to)
       };      
     }
 
-    if(params.type !== undefined && params.type !== null) {
+    if(params.type !== undefined && params.type !== null && params.type !== '') {
       cond = {
         ...cond,
         type: params.type
       }
     }
 
-    if(params.search !== undefined && params.search !== null) {
+    if(params.search !== undefined && params.search !== null && params.search !== '') {
       let utfString = params.search.toString('utf8');
       cond = {
         ...cond,
@@ -125,16 +133,87 @@ app.get('/api/listGK', (req, res) => {
           createdAt: dated
       }
     }
-  }
 
-  Tips.find(cond, function(err, data) {
-    if(err) {
-      console.log(err);
-      return res.json({ status: "error", error: err })
+    if(params.orderType !== undefined && params.orderType !== null) {
+      if(params.orderType == 'asc') {
+        orderStrategy = 1
+      }
+
+      if(params.orderType == 'desc') {
+        orderStrategy = -1
+      }
     }
 
-    return res.json({ status: "success", data })
-  })
+    if(params.orderBy !== undefined && params.orderBy !== null) {
+      switch(params.orderBy) {
+        case 'type':
+          orderCond = {
+            sort: {
+              type: orderStrategy
+            }
+          }
+
+          break;
+        case 'description':
+          orderCond = {
+            sort: {
+              description: orderStrategy
+            }
+          };
+
+          break;
+        case 'answer':
+          orderCond = {
+            sort: {
+              answer: orderStrategy
+            }
+          };
+
+          break;
+        case 'createdAt':
+          orderCond = {
+            sort: {
+              createdAt: orderStrategy
+            }
+          };
+
+          break;
+        default:
+          orderCond = {
+            sort: {
+              _id: orderStrategy
+            }
+          };
+
+          break;
+      }
+      
+    }
+  }
+
+  const totalCount = await Tips.find(cond, null, orderCond).countDocuments().exec();
+  const totalPages = Math.ceil(totalCount / pageCount);
+
+  Tips.find(cond, null, orderCond)
+      .skip(pageNumber > 0 ? ((pageNumber - 1) * pageCount) : 0)
+      .limit(pageCount)
+      .exec(function(err, data) {
+          if(err) {
+            console.log(err);
+            return res.json({ status: "error", error: err })
+          }
+
+          return res.json({ 
+            status: "success", 
+            filter: cond, 
+            ...orderCond,
+            count: totalCount,
+            totalPages: totalPages,
+            rowsPerPage: pageCount,
+            currentPage: pageNumber,
+            data 
+          })
+      })
 })
 
 // Server instance
